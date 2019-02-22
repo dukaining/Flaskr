18056a26efb1f9b84b71f86621ecb06c0ee54aac
凯宁 杜 <dukaining@live.com>
2019-02-22 17:08:57 +0800

    学习视图蓝图，增加login和register视图

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -6,9 +6,9 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
-bp = Blueprint('auth', __name__, url_defaults='/auth')
+bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register', methord=('GET', 'POST'))
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -21,7 +21,7 @@ def register():
         elif not password:
             error = 'Password is required.'
         elif db.execute(
-            'SELECT id FORM  user WHERE username=?',(username,)
+            'SELECT id FROM  user WHERE username=?',(username,)
         ).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
 
