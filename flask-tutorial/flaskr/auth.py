a1a5bb9e46ed5d61d3bd5086a3a5c64238a5f7ae
凯宁 杜 <dukaining@live.com>
2019-02-24 17:36:36 +0800

    添加增删改操作

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -82,7 +82,7 @@ def logout():
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        if g.user is none:
+        if g.user is None:
             return redirect(url_for('auth.login'))
         
         return view(**kwargs)
